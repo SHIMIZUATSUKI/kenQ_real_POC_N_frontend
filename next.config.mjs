228bea414ad3e763adbfe3c900489b8d8f922d7a
kishi_masahiro <ksmshr1988@gmail.com>
2025-08-26 10:07:08 +0900

    next.config環境変数設定

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
   experimental: {
     middlewareSourceMaps: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_AZURE_API_URL: process.env.NEXT_PUBLIC_AZURE_API_URL,
+  },
 }
 
 export default nextConfig;
